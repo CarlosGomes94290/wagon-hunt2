bc9d9b5acd9d698207ab9146f40e0573d3ea5155
Charles Madureira <madureiracharles@gmail.com>
2016-05-31 16:24:20 +0200

    creates new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/team" => "pages#team"
+  get "/contact" => "pages#join_us"
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
