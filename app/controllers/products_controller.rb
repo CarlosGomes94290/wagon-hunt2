4918010fa9662346dec4490a594d7cb9e354ab58
Charles Madureira <madureiracharles@gmail.com>
2016-06-04 19:52:05 +0200

    add resource routing for product and

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,5 +10,24 @@ class ProductsController < ApplicationController
 
   end
 
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 
 end
