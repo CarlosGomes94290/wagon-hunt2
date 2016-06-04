cca1753c1bed887133d728e37b7d19afd27930f8
Charles Madureira <madureiracharles@gmail.com>
2016-06-04 12:34:25 +0200

    create products in base

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,12 @@
 class ProductsController < ApplicationController
 
-    PRODUCTS = [
-      {name: "Kudoz", category: "Pro"},
-      {name: "uSlide", category: "Education"},
-      {name: "Hubum", category: "Lifestyle"},
-      {name: "Roadstr", category: "Lifestyle"}
-    ]
+
   def index
-    if params[:filter]
-    @products = PRODUCTS.select {|products| products[:category] == params[:filter]}
-    else
-      @products = PRODUCTS
-    end
+    @products = Product.all
   end
+
   def show
-    @product = PRODUCTS[params[:id].to_i]
+    @product = Product.find(params[:id])
 
   end
 
